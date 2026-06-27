84f5d524dff4a681bb4a31621277efaf8bd3d489
hr1th1krr <hrithikramprasath@gmail.com>
2026-06-27 18:07:08 +0100

    Refactor network interface functions and enhance socket binding with user confirmation

--- raw_engine/network_utilis.py
+++ raw_engine/network_utilis.py
@@ -2,22 +2,27 @@ import socket
 
 """ check if current network interface name is acitve or not"""
 
-def all_network_interface(interface_name):
+def all_network_interfaces(interface_name):
     try:
         network_interface_path = f"/sys/class/net/{interface_name}/operstate"  # declare directory path to the state of the current network interface
+        
         with open(network_interface_path, "r") as f:
             state = f.read().strip()  # remove unwanted elements from contents
             return state == "up"  # returns True if network interface is active
+    
     except FileNotFoundError as e:
         print("FILE COULD NOT BE FOUND")
+    
     return False  # returns False if network interface is not active
 
 """ filter out the active/running network interface"""
 
 def find_main_network_interface():
     list_all_NIC = socket.if_nameindex()  # list all available network interfaces. OUTPUT_EXAMPLE - [(index int, name string)]
+    
     for index, name in list_all_NIC:
-        if all_network_interface(name):# returns the name of NIC if current network interface is active (UP)
+        if all_network_interfaces(name):  # returns the name of NIC if current network interface is active (UP)
             return name
+    
     print("NIC not detected")
     return None
--- raw_engine/raw_packet_parser.py
+++ raw_engine/raw_packet_parser.py
@@ -15,10 +15,23 @@ if not main_network_card:  # terminate whole program if no NIC is detected
 try:
     sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(socket.ETH_P_ALL))  # establish socket to directly communicate with NIC to interpret and monitor packets.
     sniffer.bind((main_network_card, socket.ntohs(socket.ETH_P_ALL)))  # bind socket to the NIC
+    
     print(f"[*]Socket sucessfully bound to: {main_network_card}")
+    user_confirmation: str = input("Is this the correct network interface - [Y/N]: ")
+    
+    if user_confirmation.lower().strip() == "n":
+        sniffer.close()  # closes the socket
+        main_network_card = input("[*]Please enter the name of your network interface card: ")
+        
+        sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(socket.ETH_P_ALL))  # initiate a new socket 
+        sniffer.bind((main_network_card, socket.ntohs(socket.ETH_P_ALL)))  # bind the new network interface and socket
+        
+        print(f"[*]Socket is now bound to {main_network_card}")
+
 except PermissionError as p:
     print(f"[!]CRITICAL socket error: {p}")
-    print("- try run with SUDO!!!")
+    print("- Try run with SUDO!!!")
     exit()
+
 except socket.error as e:  # catch any errors related to the socket
     print(f"[!]CRITICAL socket error: {e}")
